756ff5fb60deb2f55fcc3a62313781feb216f986
Henry <thuc.tran200409@vnuk.edu.vn>
2022-07-16 12:50:38 +0700

    new update

--- DanSo/spiders/con_nhen.py
+++ DanSo/spiders/con_nhen.py
@@ -8,10 +8,16 @@ class ConNhenSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath("//h1/text()").get()
-        countries = response.xpath("//table[@id='example2']/tbody/tr/td[2]/a/text()").getall()
+        #title = response.xpath("//h1/text()").get()
+        #countries = response.xpath("//table[@id='example2']/tbody/tr/td[2]/a/text()").getall()
+        # no = response.xpath("//table[@id='example2']/tbody/tr[@class='odd']/td[@class='sorting_1']/text()").getall()
+        # population = response.xpath("//table[@id='example2']/tbody/tr/td[3]/text()").getall()
+        yearly_change = response.xpath("//table[@id='example2']/tbody/tr/td[4]/text()").getall()
 
         yield {
-            'title': title,
-            'countries': countries
+            #'title': title,
+            #'countries': countries
+            # 'no' : no
+            # 'population': population
+            'yearly_change': yearly_change
         }
